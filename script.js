2f211a2e393bbe27aa04919694994d6f0d8238a6
Rodrigues19 <rodrigueslarissacr@gmail.com>
2020-06-17 22:33:25 -0300

    definindo a direcao e fazendo aparecer no outro lado da tela

--- script.js
+++ script.js
@@ -18,26 +18,40 @@ function criandoCobrinha() {
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function play() {
+   
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   criarBG();
   criandoCobrinha();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if(direction == "right") snakeX += box;
-  if(direction == "left") snakeX -= box;
-  if(direction == "up") snakeY -= box;
-  if(direction == "down") snakeY += box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
 
   snake.pop();
 
   let newHead = {
-      x: snakeX,
-      y: snakeY
-  }
+    x: snakeX,
+    y: snakeY,
+  };
 
   snake.unshift(newHead);
-
 }
-let jog = setInterval(play, 100);
+let jogo = setInterval(play, 100);
